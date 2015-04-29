27cbd9f589a0fea5326104c4c69861dc4851625a
Kyle Gunby <gunbyk@gmail.com>
2015-04-29 10:15:42 -0500

    Added vagrant keys and some stuff

--- ServerMonkey_Mage_Box.sh
+++ ServerMonkey_Mage_Box.sh
@@ -3,10 +3,24 @@
 #Vagrant requires password-less sudo
 echo 'vagrant         ALL=(ALL)       NOPASSWD: ALL' >> /etc/sudoers
 
+#Set insecure keys
+vssh="/home/${account}/.ssh"
+mkdir -p $vssh
+chmod 700 $vssh
+(cd $vssh &&
+  wget --no-check-certificate \
+    'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' \
+    -O $vssh/authorized_keys)
+chmod 0600 $vssh/authorized_keys
+chown -R ${account}:vagrant $vssh
+unset vssh
+
+#update
+yum update -y
+
 #Install required guest additions
 yum install -y dkms binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel
 
-#Install general 
 #Add Repositories to install PHP 5.5
 rpm -Uvh https://mirror.webtatic.com/yum/el7/epel-release.rpm
 rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
